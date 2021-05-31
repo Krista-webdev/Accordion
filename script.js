eb6e062a3a0f9461ed924164dd40930860a66530
Krista Ogle <61994897+Krista-webdev@users.noreply.github.com>
2021-05-31 10:44:08 -0400

    Upload of all files

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.accordion__question').forEach((item) => {
+	item.addEventListener('click', (event) => {
+		let accCollapse = item.nextElementSibling;
+
+		if (!item.classList.contains('collapsing')) {
+			// Open accordion item
+			if (!item.classList.contains('open')) {
+				accCollapse.style.display = 'block';
+				let accHeight = accCollapse.clientHeight;
+				setTimeout(() => {
+					accCollapse.style.height = accHeight + 'px';
+					accCollapse.style.display = '';
+				}, 1);
+				accCollapse.classList = 'accordion__collapse collapsing';
+				setTimeout(() => {
+					accCollapse.classList = 'accordion__collapse collapse open';
+				}, 300);
+			}
+			// Close accordion item
+			else {
+				accCollapse.classList = 'accordion__collapse collapsing';
+				setTimeout(() => {
+					accCollapse.style.height = '0px';
+				}, 1);
+				setTimeout(() => {
+					accCollapse.classList = 'accordion__collapse collapse';
+					accCollapse.style.height = '';
+				}, 300);
+			}
+			item.classList.toggle('open');
+		}
+	});
+});
